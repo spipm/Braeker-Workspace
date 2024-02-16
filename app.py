eb7f7ff93559390b12bec88e3ee677cb8db417ab
spip <raise_issue>
2024-02-16 20:48:11 +0100

    Add factorial function and test

--- app.py
+++ app.py
@@ -16,3 +16,8 @@ def divide(x, y):
 import math
 def square_root(n):
   return math.sqrt(n)
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n-1)
--- test.py
+++ test.py
@@ -2,3 +2,5 @@ def test_true(): assert True
 from app import square_root
 def test_square_root():
   assert square_root(4) == 2.0
+def test_factorial():
+  assert factorial(5) == 120
