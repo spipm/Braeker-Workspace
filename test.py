e8f33529c1ee693aa829cd5a031557d9765fc759
spip <raise_issue>
2024-02-16 20:48:24 +0100

    Automate testing

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ def test_square_root():
   assert square_root(4) == 2.0
 def test_factorial():
   assert factorial(5) == 120
+def test_factorial_zero():
+  assert factorial(0) == 1
+import pytest
+def main():
+  pytest.main()
+if __name__ == '__main__':
+  main()
