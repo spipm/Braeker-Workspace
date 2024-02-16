48723fa9f53284c78c4d3bfd56f0bda7de4eb5fb
spip <raise_issue>
2024-02-16 20:49:00 +0100

    Create unit test for the square_number function

--- test_main.py
+++ test_main.py
@@ -12,3 +12,5 @@ class TestMain(unittest.TestCase):
     def test_divide_by_zero_error(self):
         with self.assertRaises(ValueError):
             divide_numbers(1, 0)
+    def test_square_number(self):
+        self.assertEqual(square_number(3), 9)
